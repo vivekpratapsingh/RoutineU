4734a3ec92c39127e3c287bfc3c654473c08dbae
Vivek Singh <vivsingh2@publicisgroupe.net>
2018-03-12 16:25:46 +0530

    home page diet log added

--- ui/src/pages/home/home.ts
+++ ui/src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
-import { NavController, ActionSheetController } from 'ionic-angular';
+import { NavController, ActionSheetController,NavParams,Events } from 'ionic-angular';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ExerciseSearchComponent } from '../exercise/exercise-search/exercise.search';
 import { WaterAddComponent } from '../water/water-add/water.add';
@@ -12,9 +12,21 @@ import { FoodSearchComponent } from '../food/food-search/food.search';
 })
 export class HomeComponent {
 
+    user :any;
+    date : any;
+    userLog : any;
     constructor(private navCtrl: NavController, private route: ActivatedRoute,
-        private actionSheetCtrl: ActionSheetController) { }
-
+        private actionSheetCtrl: ActionSheetController,private events : Events) {
+            this.user = this.route.snapshot.data['user'];
+            this.date = new Date();
+            this.getLogByDate();
+            this.events.subscribe('user-detail', (userdetail) => {
+                this.user = userdetail;
+                this.getLogByDate();
+            });
+         }
+        
+    // add water    
     addExercise() {
         this.route.params.subscribe(params => {
             console.log(params);
@@ -25,6 +37,8 @@ export class HomeComponent {
             }
         })
     }
+
+    // add water
     addWater() {
         this.route.params.subscribe(params => {
             console.log(params);
@@ -36,6 +50,7 @@ export class HomeComponent {
         })
     }
 
+    // select meal option
     selectMealOption() {
         let actionSheet = this.actionSheetCtrl.create({
             title: 'Meals',
@@ -72,12 +87,108 @@ export class HomeComponent {
         actionSheet.present();
     }
 
+    // add diet
     addDiet(op: string) {
         let user = this.route.snapshot.data['user'];
         if (user != undefined) {
             this.navCtrl.push(FoodSearchComponent, { user: user, date: new Date(), mealOption: op })
         }
 
+    }
+
+    // filter diet log by meal and date
+    filterDietByMeal(op): any {
+        return this.user.logs.diet.filter(log =>
+            new Date(log.added).toLocaleDateString() == this.date.toLocaleDateString() && log.mealOption == op);
+    }
+
+    // get diet log by date
+    getLogByDate() {
+        let breakfastLog = this.filterDietByMeal('Breakfast');
+        let lunchLog = this.filterDietByMeal('Lunch');
+        let dinnerLog = this.filterDietByMeal('Dinner');
+        let snacksLog = this.filterDietByMeal('Snacks');
+
+        let breakfastLogDisplay = this.getLogsWithCalories(breakfastLog);
+        let lunchLogDisplay = this.getLogsWithCalories(lunchLog);
+        let dinnerLogDisplay = this.getLogsWithCalories(dinnerLog);
+        let snacksLogDisplay = this.getLogsWithCalories(snacksLog);
+        this.userLog = {
+            exercise: this.user.logs.exercise.filter(exercise => new Date(exercise.added).toLocaleDateString() == this.date.toLocaleDateString()),
+            breakfast: breakfastLogDisplay,
+            lunch: lunchLogDisplay,
+            dinner: dinnerLogDisplay,
+            snacks: snacksLogDisplay,
+            water: this.user.logs.water.filter(log => new Date(log.added).toLocaleDateString() == this.date.toLocaleDateString())
+        }
+        this.userLog.calories = {
+            req: this.getRequiredCalories(),
+            actual: this.getActualCalories()
+        }
+    }
+    // get required calories consumed by date
+    getRequiredCalories(): any {
+        let tempReqCalories: any;
+        let reqCalories = 0;
+        tempReqCalories = this.user.logs.goal.calories.filter(cal => new Date(cal.added).toLocaleDateString() == this.date.toLocaleDateString());
+        if (tempReqCalories.length > 0) {
+            reqCalories = tempReqCalories[tempReqCalories.length - 1].calories;
+        }
+        else {
+            tempReqCalories = this.user.logs.goal.calories.filter(cal => new Date(cal.added) < this.date);
+            if (tempReqCalories.length > 0) {
+                reqCalories = tempReqCalories[tempReqCalories.length - 1].calories;
+            }
+        }
+        return reqCalories;
+    }
+
+    getActualCalories() : any{
+        let actualCal = 0;
+        if (this.userLog.breakfast) {
+            this.userLog.breakfast.log.forEach(element => {
+                actualCal = actualCal + element.calories
+            });
+        }
+        if (this.userLog.lunch) {
+            this.userLog.lunch.log.forEach(element => {
+                actualCal = actualCal + element.calories
+            });
+        }
+        if (this.userLog.dinner) {
+            this.userLog.dinner.log.forEach(element => {
+                actualCal = actualCal + element.calories
+            });
+        }
+        if (this.userLog.snacks) {
+            this.userLog.snacks.log.forEach(element => {
+                actualCal = actualCal + element.calories
+            });
+        }
+        return actualCal;
+    }
+
+    // get diet logs with calories consumed
+    getLogsWithCalories(dietLog): any {
+        let displayLog = [];
+        let calories: number = 0;
+
+        dietLog.forEach(element => {
+            let data: number = element.food.quantity.filter(q => q.serving_size.size.amount == element.servings.size.amount && q.serving_size.size.unit == element.servings.size.unit)[0].serving_size.calories * element.servings.quantity;
+            calories = calories + data;
+            displayLog.push({
+                _id: element._id,
+                food: element.food,
+                servings: element.servings,
+                mealOption: element.mealOption,
+                calories: data
+            })
+        });
+        return { log: displayLog, calories: calories };
+    }
+
+    openDiary(){
+        this.navCtrl.parent.select(1);
     }
 
 }
